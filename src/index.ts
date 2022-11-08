975fcf5678a8b43117af1f9edd8969cadde0c73b
Matheus Ribeiro <matheus.salgado@gmail.com>
2022-11-08 13:49:08 -0300

    adding typescript and express

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+    return res.send('Hi');
+});
+
+app.listen(8080);
+console.log('Application is running on port 8080');
