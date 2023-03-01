2507daf5865984b0db44d28737d59e729591fff5
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-03-01 17:31:42 -0300

    Basic abstract Entity

--- src/Domain/Aggregates/Contact/Contact.ts
+++ src/Domain/Aggregates/Contact/Contact.ts
@@ -1,21 +1,27 @@
 import DomainError from "../../Base/DomainException"
+import Entity from "../../Base/Entity"
 import Email from "../../Base/ValueObject/Email"
 import FullName from "../../Base/ValueObject/FullName"
 import Phone from "../../Base/ValueObject/Phone"
 
 type ContactAccount = Email|Phone
 
-export default class Contact {
+export default class Contact extends Entity {
   public readonly contactId: string
   public readonly fullName: FullName
   public readonly description: string
   public readonly accounts: ContactAccount[]
 
   constructor (contactId: string, fullName: FullName, description: string) {
+    super()
     this.contactId = contactId
     this.fullName = fullName
     this.description = description
     this.accounts = []
+  }
+
+  public isValid(): boolean {
+    return this.fullName.isValid() && this.accounts.length > 0
   }
 
   public addAcount(account: ContactAccount): void {
