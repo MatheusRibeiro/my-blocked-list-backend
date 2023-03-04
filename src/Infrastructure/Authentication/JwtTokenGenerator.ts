b6eb51ca0ac95c968edab463017b7cc6a0f0813b
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-03-04 01:16:09 -0300

    using jsonwebtoken lib to generate jwt

--- src/Infrastructure/Authentication/JwtTokenGenerator.ts
+++ src/Infrastructure/Authentication/JwtTokenGenerator.ts
@@ -1,10 +1,17 @@
-import { autoInjectable } from "tsyringe";
-import IJwtTokenGenerator from "../../Application/Services/Authentication/IJwtTokenGenerator";
+import IJwtTokenGenerator from "../../Application/Services/Authentication/IJwtTokenGenerator"
+import jwt from "jsonwebtoken"
 
-@autoInjectable()
+const secret = '?'
+
 export default class JwtTokenGenerator implements IJwtTokenGenerator {
     generateToken(userId: string, firstName: string, lastName: string, username: string): string {
-        return 'token generated on infra layer'
+        const payload = {
+            userId: userId,
+            username: username,
+            fullname: `${firstName} ${lastName}`
+        }
+
+        return jwt.sign(payload, secret)
     }
 
 }
