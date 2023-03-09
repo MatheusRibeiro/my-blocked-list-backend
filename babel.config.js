cb3d29089431c596407e746f6a8eee348d1c69f2
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-03-09 16:12:17 -0300

    adding detail about babel plugin dependency

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,13 @@
 module.exports = {
   plugins: [
+    //required for tsyringe (dependency injection lib)
     'babel-plugin-transform-typescript-metadata'
   ],
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    [
+      '@babel/preset-env',
+      { targets: { node: 'current' } }
+    ],
     '@babel/preset-typescript'
   ]
 }
