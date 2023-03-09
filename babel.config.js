9ee6a97437131f4b969955922709caafe13ca29d
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-03-09 16:37:40 -0300

    adding detail about babel presets

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,15 @@
 module.exports = {
   plugins: [
-    //required for tsyringe (dependency injection lib)
+    // required for tsyringe (dependency injection lib)
     'babel-plugin-transform-typescript-metadata'
   ],
   presets: [
     [
+      // allows the use the latest JS without needing to micromanage which syntax transforms are needed by your target environment
       '@babel/preset-env',
       { targets: { node: 'current' } }
     ],
+    // recommended if you use TypeScript, includes the @babel/plugin-transform-typescript plugin
     '@babel/preset-typescript'
   ]
 }
