2cf94eade40fa53093ef092d99fc74c90446a0fb
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-03-14 11:41:30 -0300

    absolute path import and linting

--- src/DependencyInjection.ts
+++ src/DependencyInjection.ts
@@ -1,15 +1,11 @@
 import { container } from 'tsyringe'
-
-import IJwtTokenGenerator from './Application/Services/Authentication/IJwtTokenGenerator'
-import JwtTokenGenerator from './Infrastructure/Authentication/JwtTokenGenerator'
-
-import IAuthenticationService from './Application/Services/Authentication/IAuthenticationService'
-import AuthenticationService from './Application/Services/Authentication/AuthenticationService'
-
-import IUserRepository from './Application/Services/Authentication/IUserRepository'
-import UserInMemoryRepository from './Infrastructure/Repositories/UserInMemoryRepository'
+import IJwtTokenGenerator from '@src/Application/Services/Authentication/IJwtTokenGenerator'
+import JwtTokenGenerator from '@src/Infrastructure/Authentication/JwtTokenGenerator'
+import IAuthenticationService from '@src/Application/Services/Authentication/IAuthenticationService'
+import AuthenticationService from '@src/Application/Services/Authentication/AuthenticationService'
+import IUserRepository from '@src/Application/Services/Authentication/IUserRepository'
+import UserInMemoryRepository from '@src/Infrastructure/Repositories/UserInMemoryRepository'
 
 container.registerSingleton<IJwtTokenGenerator>('JwtTokenGenerator', JwtTokenGenerator)
 container.registerSingleton<IAuthenticationService>('AuthenticationService', AuthenticationService)
-
 container.registerSingleton<IUserRepository>('UserRepository', UserInMemoryRepository)
