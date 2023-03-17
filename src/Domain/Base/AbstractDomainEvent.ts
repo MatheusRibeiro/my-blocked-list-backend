a0cecff65f0d12fa3ebdf4966251014e45f22539
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-03-17 12:27:26 -0300

    adding id to domain events

--- src/Domain/Base/AbstractDomainEvent.ts
+++ src/Domain/Base/AbstractDomainEvent.ts
@@ -1,11 +1,14 @@
 import Audit from './Audit'
+import UUID, { uuidFactory } from './ValueObject/UUID'
 
 export default abstract class DomainEvent<Payload> {
+    public readonly eventId: UUID
     abstract name: string
-    public readonly audit: Audit
     public readonly payload: Payload
+    public readonly audit: Audit
 
     constructor(payload: Payload, audit: Audit) {
+        this.eventId = uuidFactory()
         this.payload = payload
         this.audit = audit
     }
