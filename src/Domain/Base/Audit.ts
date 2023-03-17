c0311afd8d34e8cb9012c0e32e0779ec9ce1f413
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-03-17 14:13:27 -0300

    using milisseconds timestamp to send date on audit

--- src/Domain/Base/Audit.ts
+++ src/Domain/Base/Audit.ts
@@ -12,7 +12,7 @@ export default class Audit {
     public toJSON(): object {
         return {
             who: this.who.toJSON(),
-            when: this.when.toJSON(),
+            when: this.when.getTime(),
         }
     }
 }
