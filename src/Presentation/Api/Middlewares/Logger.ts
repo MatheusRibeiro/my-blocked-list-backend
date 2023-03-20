1bc50313ef823a1c29c26685dc083d33b7b1c9d6
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-03-19 22:55:14 -0300

    removing query string parameters from log

--- src/Presentation/Api/Middlewares/Logger.ts
+++ src/Presentation/Api/Middlewares/Logger.ts
@@ -3,6 +3,12 @@ import IMiddleware from '../Base/IMiddleware'
 
 export default class LoggerMiddleware extends IMiddleware {
     public execute = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-        console.log(`${req.method}: ${req.url}`)
+        // ?param1={param1}&param2={param2}
+        const regexMiddle = /=.*&/
+        const regexEnd = /=[^=]*$/
+        const replace = '=*****'
+        const url = req.url.replace(regexEnd, replace).replace(regexMiddle, `${replace}&`)
+
+        console.log(`${req.method}: ${url}`)
     }
 }
