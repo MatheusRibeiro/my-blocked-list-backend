b123fbab545070f717b2556986c79e38ba40f02d
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-03-29 14:29:55 -0300

    deleting dead code

--- src/Application/Base/EventEmmiter.ts
+++
@@ -1,6 +0,0 @@
-import DomainEvent from '@src/Domain/Base/AbstractDomainEvent'
-
-export default async function eventEmmiter(events: DomainEvent[]): Promise<null> {
-    events.map(event => console.log(event.constructor.name))
-    return null
-}
