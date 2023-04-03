38e19bb9fa3fad40fe6f3e6b5489cd87866930de
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-04-03 01:29:34 -0300

    cleaning up

--- src/Domain/Aggregates/WatchedContact/ValueObjects/ContactAccount.ts
+++ src/Domain/Aggregates/WatchedContact/ValueObjects/ContactAccount.ts
@@ -6,7 +6,6 @@ export type AccountType = Phone | Email
 
 export default abstract class ContactAccount<T extends AccountType = AccountType> extends ValueObject<T> {
     public isValid(): boolean {
-        console.log('########', typeof this.value)
         return true
     }
 
