39008ea48a842a0d0b55da64339206e3903b0b01
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-04-19 20:52:00 -0300

    adjusting server setup calling db connect before initializing api

--- src/Presentation/Api/server.ts
+++ src/Presentation/Api/server.ts
@@ -11,12 +11,16 @@ import './Controllers/Authentication'
 import './Controllers/Contact'
 import './Controllers/WatchedContact'
 import './Controllers/UserNotification'
+import dbConnect from '@src/Infrastructure/Storage/InMemory/Base/dbConnect'
 
-const app = express()
+void dbConnect().then(() => {
+    const app = express()
 
-app.use(json())
-app.use(router)
+    app.use(json())
+    app.use(router)
 
-const { port } = apiSettings
-app.listen(port)
-console.log(`Application is running on port ${port}`)
+    const { port } = apiSettings
+    app.listen(port)
+
+    console.log(`Application is running on port ${port}`)
+})
