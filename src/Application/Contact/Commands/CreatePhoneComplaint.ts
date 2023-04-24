8d6d75e4184552f27b30fdcceb556ddb67cfc299
Matheus Ribeiro <matheus.salgado@gmail.com>
2023-04-24 19:58:51 -0300

    validating value objects when mapping request on create phone complaint command

--- src/Application/Contact/Commands/CreatePhoneComplaint.ts
+++ src/Application/Contact/Commands/CreatePhoneComplaint.ts
@@ -7,6 +7,7 @@ import UUID from '@src/Domain/Base/ValueObject/UUID'
 import Phone from '@src/Domain/Base/ValueObject/Phone'
 import PersonName from '@src/Domain/Base/ValueObject/PersonName'
 import ContactEventDispatcher from '../Events/ContactEventsDispatcher'
+import BadRequestError from '@src/Domain/Errors/BadRequestError'
 
 interface CreatePhoneComplaintRequest {
     firstName: string
@@ -19,9 +20,28 @@ interface CreatePhoneComplaintRequest {
 }
 
 function mapper(input: CreatePhoneComplaintRequest): CreatePhoneComplaintDTO {
+    const errors: string[] = []
+
     const authorId = new UUID(input.authorId)
+    const authorIdError = authorId.getError()
+    if (authorIdError !== undefined) {
+        errors.push(authorIdError)
+    }
+
     const phone = new Phone(input.phone)
+    const phoneError = phone.getError()
+    if (phoneError !== undefined) {
+        errors.push(phoneError)
+    }
     const personName = new PersonName({ firstName: input.firstName, lastName: input.lastName })
+    const personNameError = personName.getError()
+    if (personNameError !== undefined) {
+        errors.push(personNameError)
+    }
+
+    if (errors.length > 0) {
+        throw new BadRequestError(`Invalid parameters for CreatePhoneComplaint: ${errors.join(', ')}`)
+    }
 
     return {
         personName: personName.getValidatedInstance(),
